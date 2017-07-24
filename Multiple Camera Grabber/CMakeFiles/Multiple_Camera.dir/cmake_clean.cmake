b6150261f68ba8446862073332935d9d27844c6e
Robert Hoffman <rob.hoffman@autonomousnewz.com>
2017-07-24 07:27:31 -0400

    Add files via upload

---
+++ Multiple Camera Grabber/CMakeFiles/Multiple_Camera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Multiple_Camera.dir/multiple_camera.cpp.o"
+  "Multiple_Camera.pdb"
+  "Multiple_Camera"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Multiple_Camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
